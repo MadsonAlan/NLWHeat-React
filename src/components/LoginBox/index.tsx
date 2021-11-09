729c73f1f8b53aec6ce1826003b68aa7f140abf6
MadsonAlan <madson99hitech@gmail.com>
2021-11-08 21:51:44 -0300

    retorno de mensagens do backend e capturando rash

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,26 @@
 import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
+import { useEffect } from 'react'
 export function LoginBox() {
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=45cc28e52de602246f25`
+
+    useEffect(() => {
+        const url = window.location.href
+        const hasGithubCode = url.includes('?code=')
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            window.history.pushState({}, '', urlWithoutCode)
+
+        }
+    }, [])
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>
                 Entre e compartilhe sua mensagem
             </strong>
-            <a href='#' className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Entrar com Github
             </a>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,47 +1,50 @@
 import styles from './styles.module.scss'
 
 import logoImg from '../../assets/logo.svg'
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
+type Message = {
+    id: string,
+    text: string,
+    user: {
+        name: string,
+        avatar_url: string
+    }
+}
+
 export function MessageList() {
+
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data)
+
+        })
+    }, [])
+
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        menssagem teste
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/MadsonAlan.png" alt="Madson Alan" />
-                        </div>
-                        <span>Madson Alan</span>
-                    </div>
+                {messages.map(message => {
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>
+                                {message.text}
+                            </p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
 
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        menssagem teste
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/MadsonAlan.png" alt="Madson Alan" />
-                        </div>
-                        <span>Madson Alan</span>
-                    </div>
-
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        menssagem teste
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/MadsonAlan.png" alt="Madson Alan" />
-                        </div>
-                        <span>Madson Alan</span>
-                    </div>
-
-                </li>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
